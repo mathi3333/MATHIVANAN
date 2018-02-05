210ddcd689127ff65ab08c34918f339827f23a02
mathi3333 <36067222+mathi3333@users.noreply.github.com>
2018-02-05 16:35:57 +0530

    Create new file

---
+++ mathi2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+int a;
+scanf("%d",&a);
+if(a>1)
+printf("tha number is postive");
+else 
+printf("tha number is negative");
+getch ();
+return 0:
+}
