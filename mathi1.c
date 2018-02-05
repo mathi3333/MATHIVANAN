3b18b9cadd17e77e75d29b49147f1e6cbc47ab92
mathi3333 <36067222+mathi3333@users.noreply.github.com>
2018-02-05 16:11:21 +0530

    Create mathi1.c

---
+++ mathi1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.c>
+int main()
+{
+int a,b,c;
+clrscr();
+printf("Enter the a,b,c value);
+scanf("%d%d&a&b&c");
+if(a>b>c)
+{
+printf("\n %d a is positive number%d&a&b&c");
+else
+printf("\n %d a is negative number%d&a&b&c");
+else
+printf("\n %d a is zero%d&a&b&c");
+getch();
+return 0:
+}
